570692b165c5ef671b4d5c31e94fdfc53b53896c
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-10-01 16:33:29 +0200

    feat: try to add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script>
 
+  import { Navbar } from './views/partials/navbar.vue';
+
+  export default{
+    components: {
+      Navbar,
+    }
+  }
+
 </script>
 
 <template>
 
+<div>
+  <Navbar />
   <router-view></router-view>
+</div>
 
 </template>
 
---
+++ src/views/partials/navbar.vue
@@ -0,0 +1,37 @@
+<script>
+export default{
+    name: 'Navbar',
+}
+
+</script>
+
+
+<template>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <li class="nav-item">
+          <router-link :to="{name:'home'}"></router-link>
+        </li>
+
+        <li class="nav-item">
+          <!-- <a class="nav-link" href="#">Progetti</a> -->
+          <router-link :to="{name:'projects'}"></router-link>
+        </li>
+
+      </ul>
+
+    </div>
+  </div>
+</nav>
+
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
