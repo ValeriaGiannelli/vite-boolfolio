093593163f43aeff8fad861320549362538adc10
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-10-01 17:30:05 +0200

    feat: add static paginator

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -7,6 +7,10 @@
     data(){
       return {
         projects: [],
+        paginator: {
+          current_page: 1,
+          links:[]
+        }
       }
     },
 
@@ -15,8 +19,11 @@
       getApi(){
         axios.get('http://127.0.0.1:8000/api/progetti')
               .then(result =>{
-                console.log(result.data.projects.data);
+                console.log(result.data);
                 this.projects = result.data.projects.data;
+                this.paginator.current_page = result.data.projects.current_page;
+                this.paginator.links = result.data.projects.links;
+                console.log(this.paginator);
               })
       }
     },
@@ -37,8 +44,21 @@
       <li class="list-group-item" v-for="project in projects" >{{project.title}}</li>
     </ul>
 
+    <div class="paginator">
+      <button class="btn " v-for="(link, index) in paginator.links" :key="index" v-html="link.label" :disabled="link.active"></button>
+
+    </div>
+
 </template>
 
-<style>
-
+<style lang="scss" scoped>
+  .paginator{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    button{
+      margin: 0 5px;
+      padding: 5px;
+    }
+  }
 </style>
