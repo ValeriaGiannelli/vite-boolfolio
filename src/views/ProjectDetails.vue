b6f8871cb6325f3e327d2274c6ec2b67082863ae
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-10-02 15:24:56 +0200

    feat: make a axios call to get the detail of the project and print the info

--- src/views/ProjectDetails.vue
+++ src/views/ProjectDetails.vue
@@ -1,16 +1,55 @@
+<!-- in questa pagina devo fare una chiamata all'api della pagina di dettaglio -->
+
 <script>
+import axios from 'axios';
+import {store} from '../store.js';
 
 export default{
-    name: 'ProjectDetails'
+    name: 'ProjectDetails',
+
+    data(){
+        return {
+           //salvo le info del progetto 
+           project: {} 
+        }
+    },
+
+    methods: {
+        getApi(slug){
+            axios.get(store.apiUrl + 'progetto/' + slug)
+                .then(result =>{
+                    console.log(result.data);
+                    // se il progetto esiste ancora
+                    if(result.data.success){
+                        this.project = result.data.project;
+                    } else {
+                        console.log('Errore 404');    
+                    }
+                    
+                })
+        },
+
+        // modifica della data
+        dateFormat(date){
+            return new Date(date).toLocaleDateString();
+        }
+    },
+
+    mounted(){
+        // console.log(this.$route.params.slug);
+        const slug = this.$route.params.slug;
+        
+        this.getApi(slug);
+    }
 }
 
 </script>
 
 <template>
-    <h1>Dettaglio progetto: Nome progetto</h1>
+    <h1>{{project.title}}</h1>
     <img src="" alt="posto dell'immagine">
-    <p>DESCRIZIONE</p>
-    <P>Data inizio: </P>
+    <p>{{project.description}}</p>
+    <P>Data inizio: {{dateFormat(project.start_date)}} </P>
     <p>Linguaggi usati</p>
 
 </template>
